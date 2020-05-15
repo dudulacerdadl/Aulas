705cf41cf182948cce39b8213b68cd33953c3704
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-05-15 01:12:05 -0300

    Atualização

---
+++ Primeiro Ano/Lógica de Programação/Java/src/Aula2/Exemplo3.java
@@ -0,0 +1,21 @@
+package Aula2;
+
+import java.util.Scanner;
+
+public class Exemplo3 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.print("Número:");
+            numero = teclado.nextInt();
+            if (numero % 2 == 0){
+                System.out.println("É par");
+            } else {
+                System.out.println("É ímpar");
+            }
+        }
+    }
+}
