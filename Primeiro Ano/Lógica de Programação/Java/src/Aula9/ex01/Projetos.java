a36cf7fd12b757a81ee8eba868aa301023432355
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-06-13 02:34:29 -0300

    Atualização

--- Primeiro Ano/Lógica de Programação/Java/src/Aula9/ex01/Projetos.java
+++ Primeiro Ano/Lógica de Programação/Java/src/Aula9/ex01/Projetos.java
@@ -5,45 +5,38 @@ public class Projetos {
     private double custoHora;
     private String nome;
 
-    public Projetos (String nome){
-        this.nome = nome;
+    public Projetos (String novoNome){
+        this.nome = novoNome;
         this.custoHora = 10.5;
     }
-
-    public Projetos (String nome, double custoHora){
-        this.nome = nome;
-        this.custoHora = custoHora;
+    public Projetos (String novoNome, double novoCustoHora){
+        this.nome = novoNome;
+        this.custoHora = novoCustoHora;
     }
-
-    public void registrarHoras(int hora){
-        this.hora += hora;
-    }
-
-    public double getCustoAtual(){
-        return (this.hora * this.custoHora);
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-
-    public double getCustoHora(){
-        return this.custoHora;
-    }
-
     public int getHora(){
         return this.hora;
     }
-
+    public double getCustoHora(){
+        return this.custoHora;
+    }
+    public String getNome(){
+        return this.nome;
+    }
     public void setNome(String novoNome){
         if (!novoNome.equals("")){
             this.nome = novoNome;
         }
     }
-
     public void setCustoHora(double novoCustoHora){
         if (novoCustoHora > 0){
             this.custoHora = novoCustoHora;
         }
+    }
+
+    public void registrarHoras(int novaHora){
+        this.hora += novaHora;
+    }
+    public double getCustoAtual(){
+        return (this.hora * this.custoHora);
     }
 }
