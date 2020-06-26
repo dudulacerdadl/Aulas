60d8bf17948c3d71570cf200c5ddeb65cddbb97b
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-06-26 20:40:01 -0300

    Atualização

---
+++ Primeiro Ano/Lógica de Programação/Java/src/AulaFinal/CartaoCredito.java
@@ -0,0 +1,12 @@
+package AulaFinal;
+
+public class CartaoCredito {
+    private int Numero;
+    private float Saldo;
+    private float Limite;
+    private float Bonus;
+    private String Bandeira;
+
+    public CartaoCredito (int novoNumero, String novaBandeira){}
+    public CartaoCredito (int novoNumero, float novoLimite, String novaBandeira){}
+}
